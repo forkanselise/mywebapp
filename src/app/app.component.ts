c5844b2fd9cd13044a648c384ef5db8d0e494af4
forkanselise <thouhidur.forkan@selise.ch>
2023-05-05 02:30:11 +0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-videoplayer-app';
+
+  playlist = [
+    {
+      title: 'Agent 327!',
+      src: 'https://media.vimejs.com/720p.mp4',
+      type: 'video/mp4'
+    }
+  ];
+
+  currentIndex = 0;
+  activeVideo = this.playlist[this.currentIndex];
+  api!: { getDefaultMedia: () => { (): any; new(): any; subscriptions: { (): any; new(): any; loadedMetadata: { (): any; new(): any; subscribe: { (arg0: () => void): void; new(): any; }; }; ended: { (): any; new(): any; subscribe: { (arg0: () => void): void; new(): any; }; }; }; }; play: () => void; };
+
+  constructor() {
+  }
+
+  counter(size: number) {
+    return new Array(size);
+  }
+
+  onPlayerSet(api: any) {
+    this.api = api;
+
+    this.api.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.startVideo.bind(this));
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+  }
+
+  nextVideo() {
+    this.currentIndex++;
+
+    if (this.currentIndex === this.playlist.length) {
+      this.currentIndex = 0;
+    }
+
+    this.activeVideo = this.playlist[this.currentIndex];
+  }
+
+  startVideo() {
+    this.api.play();
+  }
+
+  onClickPlaylistVideo(item: { title: string; src: string; type: string; }, index: number) {
+    this.currentIndex = index;
+    this.activeVideo = item;
+  }
+
+}
